17c35458c2a872fffa44d7897824da10bcb16d74
jeltedeproft <jdeproft@vub.ac.be>
2017-09-15 01:51:23 +0200

    added project to github

---
+++ game/dota_addons/iseedeadpeople/scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,14 @@
+-- This is the entry-point to your game mode and should be used primarily to precache models/particles/sounds/etc
+
+require('internal/util')
+require('gamemode')
+
+function Precache( context )
+
+end
+
+-- Create the game mode when we activate
+function Activate()
+  GameRules.GameMode = GameMode()
+  GameRules.GameMode:_InitGameMode()
+end
